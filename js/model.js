df25faaf69846b79b82b13807c59d1b9dd79a7bf
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-06-30 15:45:00 +0530

    Polish POS product grid layout

--- js/model.js
+++ js/model.js
@@ -1162,3 +1162,4 @@ const Model = {
 // maintenance: Refine discount application rules (2025-06-28)
 // maintenance: Refine order history pagination (2025-06-29)
 // maintenance: Refine product search filters (2025-06-29)
+// maintenance: Polish POS product grid layout (2025-06-30)
