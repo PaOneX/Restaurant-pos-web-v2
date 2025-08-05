06737d48e16e126dcca7bc1d865ebeea64609a5e
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-08-05 21:00:00 +0530

    Polish responsive breakpoints

--- js/app.js
+++ js/app.js
@@ -134,3 +134,4 @@ window.addEventListener('afterprint', () => {
 // maintenance: Improve receipt print formatting (2025-08-04)
 // maintenance: Refine order total calculation (2025-08-04)
 // maintenance: Polish modal dialog behavior (2025-08-05)
+// maintenance: Polish responsive breakpoints (2025-08-05)
