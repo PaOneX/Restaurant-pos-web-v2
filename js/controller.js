0d53d1f29e09a4dc86058769578c2ea6a35eb184
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-08-18 18:24:00 +0530

    Adjust service charge logic

--- js/controller.js
+++ js/controller.js
@@ -1270,3 +1270,4 @@ const Controller = {
 // maintenance: Refine order total calculation (2025-08-16)
 // maintenance: Polish modal dialog behavior (2025-08-17)
 // maintenance: Polish responsive breakpoints (2025-08-17)
+// maintenance: Adjust service charge logic (2025-08-18)
