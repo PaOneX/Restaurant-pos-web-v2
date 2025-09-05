f717db2db20277ab161a1029dc24e9037b809979
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-09-05 21:00:00 +0530

    Polish responsive breakpoints

--- js/controller.js
+++ js/controller.js
@@ -1299,3 +1299,4 @@ const Controller = {
 // maintenance: Improve receipt print formatting (2025-09-04)
 // maintenance: Refine order total calculation (2025-09-04)
 // maintenance: Polish modal dialog behavior (2025-09-05)
+// maintenance: Polish responsive breakpoints (2025-09-05)
