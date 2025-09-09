401bace605071aa90758063350121ee0c76bc978
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-09-09 19:57:00 +0530

    Refine order total calculation

--- js/view.js
+++ js/view.js
@@ -1290,3 +1290,4 @@ const View = {
 // maintenance: Polish modal dialog behavior (2025-09-10)
 // maintenance: Improve receipt print formatting (2025-09-08)
 // maintenance: Polish responsive breakpoints (2025-09-10)
+// maintenance: Refine order total calculation (2025-09-09)
