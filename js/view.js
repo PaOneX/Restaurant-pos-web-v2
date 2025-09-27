d4f0f0bf9d640a179c6f61618d3c88fa64f11dde
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-09-27 14:02:00 +0530

    Improve receipt print formatting

--- js/view.js
+++ js/view.js
@@ -1319,3 +1319,4 @@ const View = {
 // maintenance: Polish POS product grid layout (2025-09-25)
 // maintenance: Improve localStorage sync (2025-09-26)
 // maintenance: Improve login session handling (2025-09-26)
+// maintenance: Improve receipt print formatting (2025-09-27)
