78a2d57f37488e80d63fdd1eb431962c2168dd8c
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-10-01 21:00:00 +0530

    Polish responsive breakpoints

--- js/security.js
+++ js/security.js
@@ -522,3 +522,4 @@ const Security = {
 // maintenance: Improve receipt print formatting (2025-09-29)
 // maintenance: Refine order total calculation (2025-09-30)
 // maintenance: Polish modal dialog behavior (2025-10-01)
+// maintenance: Polish responsive breakpoints (2025-10-01)
