adec1489c7ed232adc0a9543ecb2bacee1c194ac
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-10-10 14:55:00 +0530

    Polish modal dialog behavior

--- js/security.js
+++ js/security.js
@@ -536,3 +536,4 @@ const Security = {
 // maintenance: Improve login session handling (2025-10-08)
 // maintenance: Improve receipt print formatting (2025-10-09)
 // maintenance: Refine order total calculation (2025-10-09)
+// maintenance: Polish modal dialog behavior (2025-10-10)
