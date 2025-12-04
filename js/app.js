c44836238a1cffe28d737bbb2508947fddba48ad
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2025-12-04 22:24:00 +0530

    Refine order total calculation

--- js/app.js
+++ js/app.js
@@ -338,3 +338,4 @@ window.addEventListener('afterprint', () => {
 // maintenance: Improve localStorage sync (2025-12-02)
 // maintenance: Improve login session handling (2025-12-03)
 // maintenance: Improve receipt print formatting (2025-12-04)
+// maintenance: Refine order total calculation (2025-12-04)
