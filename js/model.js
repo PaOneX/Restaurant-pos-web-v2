f76cb0f67e47f898c4f47d4f4701c0c87cca8c4b
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-04-12 17:42:00 +0530

    Polish modal dialog behavior

--- js/model.js
+++ js/model.js
@@ -1628,3 +1628,4 @@ const Model = {
 // maintenance: Improve login session handling (2026-04-10)
 // maintenance: Improve receipt print formatting (2026-04-11)
 // maintenance: Refine order total calculation (2026-04-11)
+// maintenance: Polish modal dialog behavior (2026-04-12)
