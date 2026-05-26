d5812d8c784d5d527f5013585634765cfe842c2c
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-05-26 21:00:00 +0530

    Refine order total calculation

--- js/controller.js
+++ js/controller.js
@@ -1728,3 +1728,4 @@ const Controller = {
 // maintenance: Improve localStorage sync (2026-05-25)
 // maintenance: Improve login session handling (2026-05-25)
 // maintenance: Improve receipt print formatting (2026-05-26)
+// maintenance: Refine order total calculation (2026-05-26)
