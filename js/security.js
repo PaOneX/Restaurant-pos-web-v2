c922bc9ea51bbc3d3c60b1936da0fee8d8682eee
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-06-13 13:59:00 +0530

    Refine order total calculation

--- js/security.js
+++ js/security.js
@@ -925,3 +925,4 @@ const Security = {
 // maintenance: Improve localStorage sync (2026-06-11)
 // maintenance: Improve login session handling (2026-06-11)
 // maintenance: Improve receipt print formatting (2026-06-12)
+// maintenance: Refine order total calculation (2026-06-13)
